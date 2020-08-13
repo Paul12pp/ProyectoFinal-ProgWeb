79c165f76f45145f975f06da7003a6c807e06402
Paul Abel Sanchez <psanchez@oriontek.do>
2020-08-12 22:17:56 -0400

    Base datos, modelos y controladores.

---
+++ ProyectoFinal/Models/Gasto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal.Models
+{
+    public class Gasto
+    {
+        [Key]
+        public int IdGasto { get; set; }
+        public DateTime Fecha { get; set; }
+        public int IdConsumo { get; set; }
+        public decimal Monto { get; set; }
+        public int IdPago { get; set; }
+        public string IdUsuario { get; set; }
+        public string Observacion { get; set; }
+    }
+}
